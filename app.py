6a3d61d7872f1213c98e8d84a08ca27c51ea9f39
Aryan Sharma <50998117+aryan2909@users.noreply.github.com>
2023-07-02 22:14:46 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# MongoDB configuration
+
 client = MongoClient('mongodb://localhost:27017/')
 db = client['form_db']
 collection = db['form_data']
@@ -11,11 +11,11 @@ collection = db['form_data']
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Get form data
+
         name = request.form['name']
         email = request.form['email']
 
-        # Save data to MongoDB
+
         data = {'name': name, 'email': email}
         collection.insert_one(data)
 
